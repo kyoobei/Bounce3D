59cb8c20cdea1873f9aa2ce12ae312cfd71626ce
kyoobei <kyoobei01@gmail.com>
2020-12-23 17:59:42 +0800

    added bullets on shooters

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private const float TIMER_RETURN = 3f;
+    [SerializeField] private float m_bulletStrength = 5f;
+    [SerializeField] private Rigidbody m_bulletRigidBody;
+    private Pooler m_pooler;
+    public void SetPoolerForReturning(Pooler pooler)
+    {
+        m_pooler = pooler;
+    }
+    public void Shoot()
+    {
+        m_bulletRigidBody.AddForce(Vector3.forward * m_bulletStrength, ForceMode.Impulse);
+        Invoke("Return", TIMER_RETURN);
+    }
+    private void Return()
+    {
+        m_pooler.ReturnClone(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/GuardShooter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardShooter : MonoBehaviour
+{
+    [SerializeField] private Pooler m_bulletPooler = null;
+    [SerializeField] private Transform m_bulletPosition =  null;
+    [SerializeField] private float m_shootingInterval = 0.0f;
+    private float m_intervalHolder;
+    private void Start()
+    {
+        m_intervalHolder = m_shootingInterval;
+    }
+    private void Update()
+    {
+        if(m_shootingInterval > 0)
+        {
+            m_shootingInterval -= Time.deltaTime;
+        }
+        else if(m_shootingInterval <= 0)
+        {
+            ReleaseBullet();
+            m_shootingInterval = m_intervalHolder;
+        }
+    }
+    private void ReleaseBullet()
+    {
+        GameObject go = m_bulletPooler.GetClone();
+        go.transform.position = m_bulletPosition.position;
+        Bullet bullet = go.GetComponent<Bullet>();
+        bullet.SetPoolerForReturning(m_bulletPooler);
+        bullet.Shoot();      
+    }
+}
